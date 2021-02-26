1a04424c1c95bdc518c0a272f1e430e443362b2f
kgseng <sengk@oregonstate.edu>
2021-02-25 18:55:24 -0800

    Create updateEmployer.js

---
+++ public/javascript/updateEmployer.js
@@ -0,0 +1,10 @@
+function updateEmployer(employerID){
+    $.ajax({
+        url: '/Employers/' + employerID,
+        type: 'PUT',
+        data: $('#update-employer').serialize(),
+        success: function(result){
+            window.location.replace("./");
+        }
+    })
+};
