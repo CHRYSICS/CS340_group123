c3b1260814223b03a58181f69bb91e7c07b3bd2a
Chris Eckerson <44328786+CHRYSICS@users.noreply.github.com>
2021-03-15 18:28:22 -0700

    removed personal info for database, made generic

--- config/dbcon.js
+++ config/dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_eckersoc',
-  password        : 'group#123',
-  database        : 'cs340_eckersoc'
+  user            : 'enter_username',
+  password        : 'enter_password',
+  database        : 'enter_database_name'
 });
 
 module.exports.pool = pool;
